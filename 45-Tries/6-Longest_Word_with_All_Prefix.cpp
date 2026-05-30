409a18ff43694d6eb0daa64943a6d8c03d8adf06
Aditya Raj Gupta <adityarajgupta13042006@gmail.com>
2026-05-30 19:17:38 +0530

    Added Progress

---
+++ 45-Tries/6-Longest_Word_with_All_Prefix.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    unordered_map<char,Node*>children;
+    bool endOfWord;
+
+    Node(){
+        endOfWord=false;
+    }
+};
+
+class Trie{
+    Node* root;
+public:
+    Trie(){
+        root = new Node();
+    }
+
+    void insert(string key){ //TC = O(L)
+        Node* temp = root;
+        for(char ch : key){
+            if(temp->children.count(ch)==0){
+                temp->children[ch]=new Node();//insert
+            }
+            temp = temp->children[ch];//level update
+        }
+        temp->endOfWord=true;//end of key
+    }
+
+    bool search(string key){
+        Node* temp = root;
+        for(char ch: key){
+            if(temp->children.count(ch)){
+                temp=temp->children[ch];
+            }else{
+                return false;
+            }
+        }
+        return temp->endOfWord;
+    }
+
+    void longestHelper(Node* root , string&ans , string temp){
+        for(auto child : root->children){
+            if(child.second->endOfWord){
+                temp+=child.first;
+                if((temp.size()==ans.size() && temp<ans) || (temp.size()>ans.size())){
+                    ans = temp;
+                }
+                longestHelper(child.second,ans,temp);
+                temp = temp.substr(0,temp.size()-1);
+            }
+        }
+    }
+    string longestStringwithEOW(){
+        string ans = "";
+        longestHelper(root,ans,"");
+        return ans;
+    }
+};
+
+string longestString(vector<string>dict){
+    Trie trie;
+    for(string s :dict){
+        trie.insert(s);
+    }
+    return trie.longestStringwithEOW();
+}
+
+int main(){
+    vector<string>dict = {"a","banana","app","appl","ap","apply","apple"};
+    cout << longestString(dict);
+    return 0;
+}
