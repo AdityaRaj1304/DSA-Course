d0d8bb53fb832e28d9f36190cbf620bb3a7af7f6
Aditya Raj Gupta <adityarajgupta13042006@gmail.com>
2026-08-13 01:46:21 +0530

    Add implementation for Max Area of Island problem and fix Disjoint Set class

--- 50-Graphs (Part-5)/LeetCode/5)1061-Lexicographically_Smallest_Equivalent_String.cpp
+++ 50-Graphs (Part-5)/LeetCode/5)1061-Lexicographically_Smallest_Equivalent_String.cpp
@@ -2,17 +2,49 @@
 using namespace std;
 using ll = long long;
 
+class DisjointSet{
+public:
+    int n ;
+    vector<int>par;
 
-class Solution {
-    vector<int>par(26);
+    DisjointSet(int n){
+        this->n=n;
+        for(int i=0;i<n;i++){
+            par.push_back(i);
+        }
+    }
+
     int find(int x){
         if(par[x]==x){
             return x;
         }
-        return 
+        return par[x]=find(par[x]);
     }
+    
+    void unionSet(int u,int v){
+        int parU = find(u);
+        int parV = find(v);
+        if(parU==parV){
+            return;
+        }else if(parU<parV){
+            par[parV]=parU;
+        }else{
+            par[parU]=parV;
+        }
+    }
+};
+
+class Solution {
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        
+        DisjointSet dsu(26);
+        for(int i =0;i<s1.size();i++){
+            dsu.unionSet(s1[i]-'a',s2[i]-'a');
+        }
+        string ans="";
+        for(char ch:baseStr){
+            ans+=(char)dsu.find(ch-'a')+'a';
+        }
+        return ans;
     }
 };
